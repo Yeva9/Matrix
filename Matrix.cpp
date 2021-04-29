3d6baf210573b03d66f688ad60f976f97e7c8911
Yeva <eva.hovnanyan.1999@gmail.com>
2021-04-29 21:51:36 +0400

    Fixed codding style.

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,112 +1,118 @@
 #include "Matrix.h"
 
-std::ostream& operator<<(std::ostream& o, const Matrix& M)
-{
-    for(int i = 0; i < M.m_rows; ++i)
-   {
-       for(int j = 0; j < M.m_columns; ++j)
-       o<<M.m_matrix[i][j]<<' ';
+std::ostream& operator<<(std::ostream& o, const Matrix& M) {
+    for (int i = 0; i < M.m_rows; ++i) {
+       for(int j = 0; j < M.m_columns; ++j) {
+       o << M.m_matrix[i][j] << ' ';
+	   }
        //m_matrix-y private e u cheinq karogh dimel ete friend chliner
-   o<<endl;
+   o << endl;
    }
    return o;
 }
 
-std::istream& operator>>(std::istream& k, Matrix& M)
-{
-    for(int i = 0; i < M.m_rows; ++i)
-        for(int j = 0; j < M.m_columns; ++j)
-            k>>M.m_matrix[i][j];
+std::istream& operator>>(std::istream& k, Matrix& M) {
+    for (int i = 0; i < M.m_rows; ++i) {
+        for (int j = 0; j < M.m_columns; ++j) {
+            k >> M.m_matrix[i][j];
+		}
+	}
       
       return k;
 }
 
-void Matrix:: allocate() 
-{ 
-m_matrix = new int*[m_rows]; 
-for(int i = 0; i < m_rows; ++i) 
-m_matrix[i] = new int [m_columns]; 
+void Matrix:: allocate()  { 
+	m_matrix = new int*[m_rows]; 
+
+	for (int i = 0; i < m_rows; ++i)  {
+		m_matrix[i] = new int [m_columns];
+	}	
 } 
 
-void Matrix:: dellocate() 
-{ 
-for(int i = 0; i < m_rows; ++i) 
-delete [] m_matrix[i]; 
-delete [] m_matrix; 
+void Matrix:: dellocate()  { 
+	for (int i = 0; i < m_rows; ++i) {
+		delete [] m_matrix[i]; 
+	}
+	delete [] m_matrix; 
 } 
 
 Matrix::Matrix(int m, int n) 
-:m_rows(n) 
-,m_columns(m) 
-{ 
-allocate(); 
-for(int i = 0; i < n; ++i) 
-    for(int j = 0; j < m; ++j) 
-        m_matrix[i][j] = 0; 
+	: m_rows(n) 
+	, m_columns(m) { 
+	allocate(); 
+
+	for (int i = 0; i < n; ++i) {
+    	for (int j = 0; j < m; ++j) { 
+        	m_matrix[i][j] = 0; 
+		}
+	}
 } 
 
 Matrix::Matrix(int n, int m, const int** matrix) 
-:m_rows(n) 
-,m_columns(m) 
-{ 
-allocate(); 
-for(int i = 0; i < n; ++i) 
-    for(int j = 0; j < m; ++j) 
-        m_matrix[i][j] = matrix[i][j]; 
+	: m_rows(n) 
+	, m_columns(m)  { 
+	allocate(); 
+
+	for (int i = 0; i < n; ++i) {
+    	for (int j = 0; j < m; ++j) { 
+        	m_matrix[i][j] = matrix[i][j];
+		}
+	}	
 } 
 
 Matrix::Matrix(const Matrix& m) 
-:m_rows(m.m_rows) 
-,m_columns(m.m_columns) 
-{ 
-allocate(); 
-for(int i = 0; i < m_rows; ++i) 
-    for(int j = 0; j < m_columns; ++j) 
-        m_matrix[i][j] = m.m_matrix[i][j]; 
+	: m_rows(m.m_rows) 
+	, m_columns(m.m_columns)  { 
+	allocate(); 
+
+	for (int i = 0; i < m_rows; ++i) {
+    	for (int j = 0; j < m_columns; ++j) { 
+        	m_matrix[i][j] = m.m_matrix[i][j];
+		}
+	}	
 } 
 
-Matrix:: ~Matrix() 
-{ 
-dellocate(); 
+Matrix:: ~Matrix() { 
+	dellocate(); 
 } 
 
-void Matrix::add_row(const int* A, int k) //A-n ekats toghn e 
-{ 
-int ** A1 = new int * [m_rows +1]; 
-for(int j = 0; j < k; ++j)//0ic k sagh copy arecinq 
-{ 
-A1[j] = m_matrix[j]; 
+void Matrix::add_row(const int* A, int k) { //A-n ekats toghn e   
+	int ** A1 = new int * [m_rows +1]; 
+
+	for (int j = 0; j < k; ++j) {//0ic k sagh copy arecinq 
+	A1[j] = m_matrix[j]; 
+	} 
+
+	A1[k] = new int [m_columns];//k-rd y new arecinq u mez ekatsyay A1-um grecinq
+
+	for (int j = 0; j < m_columns; ++j) {
+		A1[k][j] = A[j]; 
+	}
+
+	for (int j = 0; j < m_rows +1; ++j) { 
+		A1[j] = m_matrix[j]; 
+	}
+
+	++m_rows; 
+	delete []m_matrix; 
+	m_matrix = A1; 
 } 
 
-A1[k] = new int [m_columns];//k-rd y new arecinq u mez ekatsyay A1-um grecinq
-for(int j = 0; j < m_columns; ++j) 
-A1[k][j] = A[j]; 
-
-for(int j = 0; j < m_rows +1; ++j) 
-A1[j] = m_matrix[j]; 
-++m_rows; 
-delete []m_matrix; 
-m_matrix = A1; 
+void Matrix:: remove_row(int i)  { 
+	int ** A1 = new int * [m_rows-1]; 
+	
+	for (int j = 0; j < i; ++j) { 
+    	A1[j] = m_matrix[j]; 
+	} 
+	delete []m_matrix; 
+	m_matrix = A1; 
 } 
 
-void Matrix:: remove_row(int i) 
-{ 
-int ** A1 = new int * [m_rows-1]; 
-for(int j = 0; j < i; ++j) 
-{ 
-    A1[j] = m_matrix[j]; 
-} 
-delete []m_matrix; 
-m_matrix = A1; 
+int Matrix::get_element(int i,int j) const  { 
+	return m_matrix[i][j]; 
 } 
 
-int Matrix::get_element(int i,int j) const 
-{ 
-return m_matrix[i][j]; 
-} 
-
-int Matrix::get_max() const 
-{ 
+int Matrix::get_max() const { 
 int max = 0; 
 max = m_matrix[0][0]; 
 
@@ -309,10 +315,10 @@ int main()
  B.print();
  cout << B.get_rows() << ' ' << B.get_columns() << ' ' << B.get_max() << ' ' << B.get_element(1,1) << ' ';*/ 
      
-  /*Matrix M(3,3);
+  Matrix M(3,3);
   cout << M << endl;
   cin >> M;
-  cout << endl << M;*/
+  cout << endl << M;
       
      return 0;
 
